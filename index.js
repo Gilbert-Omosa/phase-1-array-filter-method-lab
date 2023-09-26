b83ea01893522e62100ea7f553c79937d4b8d9cb
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-26 12:35:21 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toUpperCase() === string.toUpperCase());
+  }
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.startsWith(string));
+  }
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  
